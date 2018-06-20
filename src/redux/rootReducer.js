e3a7697f161990f8064b144356fd47ea0bf68a4c
Rudy Huynh <rudy.huynh286@gmail.com>
2018-06-20 13:54:44 +0700

    re-test prettier

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,9 +1,9 @@
-import { combineReducers } from 'redux'
-import * as demo from '../modules/demo'
+import { combineReducers } from 'redux';
+import * as demo from '../modules/demo';
 /**
  * This place is to register all reducers of the app.
  */
 
 export default combineReducers({
-    demo: demo.demoReducer
-})
+  demo: demo.demoReducer
+});
